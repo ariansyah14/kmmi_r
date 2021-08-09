13c1db5d106ca4dd8806837157d56955831793a0
ariansyah14 <112201906146@mhs.dinus.ac.id>
2021-08-09 16:52:09 +0700

    Add text line 2

--- hello.r
+++ hello.r
@@ -1,8 +1,9 @@
 teks1 = "Selamat datang di dunia R"
-teks2 = "Anda akan terkesima dengan kehebatannya"
+teks2 = "Anda akan terkesima dengan kehebatannya!!!!"
 
 teks1
 teks2
 
 list1 = list("Gado-gado", "Pecel Lele", "Soto Ayam")
 list1
+
